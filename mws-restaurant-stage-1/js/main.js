28fc00b1b7bc828c969555619e630852c3d5a792
Peter Clerkin <clerkin07@gmail.com>
2018-06-28 17:22:53 +0100

    Add files via upload

--- mws-restaurant-stage-1/js/main.js
+++ mws-restaurant-stage-1/js/main.js
@@ -89,19 +89,6 @@ initMap = () => {
   updateRestaurants();
 }
 
-/*window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-  updateRestaurants();
-} */
-
 /**
  * Update page and map for current restaurants.
  */
@@ -183,17 +170,6 @@ createRestaurantHTML = (restaurant) => {
 
   return li
 }
-
-/*addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
 
 /**
  * Add markers for current restaurants to the map.
